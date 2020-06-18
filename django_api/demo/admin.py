c23655d39273f0a4a0a97538856b1f806fca3623
AvinGao <15604225282@163.com>
2020-06-18 18:14:11 +0800

    deleter django_api

--- django_api/demo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-# from TestModel.models import Test
-from demo.models import Test
-from demo.models import Book
-# Register your models here.
-admin.site.register(Test)
-admin.site.register(Book)
--- django_api/testPostApi.py
+++
@@ -1,4 +0,0 @@
-import requests
- 
-res = requests.post('http://127.0.0.1:15000/postAPI/', data={'a':3, 'b':4})
-print(res.text)
